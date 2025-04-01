ef7cf85d84f7da6a9526fdc601d8a59484097b99
harperaustin <harper_austin@brown.edu>
2025-04-01 15:40:08 -0400

    starting out

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Harper Austin</h1>
       </header>
+
+      <div className="content">
+        <section id="about" className="section">
+          <h3>About Me</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia nisi vitae odio tempus, at dapibus felis gravida.
+          </p>
+          <p>
+            More details about me... Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Vivamus lacinia nisi vitae odio tempus.
+          </p>
+        </section>
+
+        <section id="projects" className="section">
+          <h3>Projects</h3>
+          <p>Get Productive: </p>
+        </section>
+      </div>
     </div>
   );
 }
